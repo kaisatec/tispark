be2ca9bc8e001275591a75ec0d0635422873c6cb
marsishandsome <marsishandsome@gmail.com>
2020-09-17 15:34:45 +0800

    tmp

--- core/src/main/scala/com/pingcap/tispark/write/TiBatchWriteTable.scala
+++ core/src/main/scala/com/pingcap/tispark/write/TiBatchWriteTable.scala
@@ -110,7 +110,7 @@ class TiBatchWriteTable(
   def preCalculate(startTimeStamp: TiTimestamp): RDD[(SerializableKey, Array[Byte])] = {
     val sc = tiContext.sparkSession.sparkContext
 
-    df.explain(true)
+    //df.explain(true)
 
     val count = df.count
     logger.info(s"source data count=$count")
